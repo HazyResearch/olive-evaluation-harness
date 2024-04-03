7eeb81a1b603bf6ab059cf3a2d69d8b6764d7dd8
Jon Saad-Falcon <jonsaadfalcon@DN51s498.SUNet>
2024-04-02 21:29:20 -0700

    update olive eval submodule

--- lm_eval/models/moe.py
+++ lm_eval/models/moe.py
@@ -10,7 +10,7 @@ import os
 import sys
 import torch 
 
-from lm_eval.train.config import Config # TODO: Uses copy of train directory from Olive! Need to fix
+from train.config import Config
 
 @register_model("moe")
 class MoELMWrapper(HFLM):
