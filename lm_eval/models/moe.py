e8909f9f9ac8ead6c2ae9f76b98f2a873d26cfb5
Jon Saad-Falcon <jonsaadfalcon@DN51s498.SUNet>
2024-04-02 21:41:25 -0700

    update olive eval submodule

--- lm_eval/models/moe.py
+++ lm_eval/models/moe.py
@@ -10,9 +10,9 @@ import os
 import sys
 import torch 
 
-breakpoint()
+#breakpoint()
 
-from train.config import Config
+from ..train.config import Config
 
 @register_model("moe")
 class MoELMWrapper(HFLM):
