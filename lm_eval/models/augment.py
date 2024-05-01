bb5c6d4ef864a33142e7d17195f3d7c67a37a9ce
eyuboglu <eyuboglu@stanford.edu@cr-a100-80-0016.identity.together.xyz>
2024-05-01 20:00:19 +0000

    hotpotqa update

--- lm_eval/models/augment.py
+++ lm_eval/models/augment.py
@@ -175,21 +175,21 @@ from transformers.activations import ACT2FN
 from transformers.modeling_outputs import BaseModelOutputWithPast, CausalLMOutputWithPast, SequenceClassifierOutputWithPast, MaskedLMOutput
 from transformers.modeling_utils import PreTrainedModel
 from transformers.pytorch_utils import ALL_LAYERNORM_LAYERS
-#from transformers.utils import (
-#    add_start_docstrings,
-#    add_start_docstrings_to_model_forward,
-#    is_flash_attn_available,
-#    logging,
-#    replace_return_docstrings,
-#    ModelOutput,
-#)
+from transformers.utils import (
+    add_start_docstrings,
+    add_start_docstrings_to_model_forward,
+    #is_flash_attn_available,
+    logging,
+    replace_return_docstrings,
+    ModelOutput,
+)
 from transformers.models.llama.configuration_llama import LlamaConfig
 
 
-if is_flash_attn_available():
-    from flash_attn import flash_attn_func, flash_attn_varlen_func
-    from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
-    from flash_attn.layers.rotary import apply_rotary_emb_func
+#if is_flash_attn_available():
+#    from flash_attn import flash_attn_func, flash_attn_varlen_func
+#    from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
+#    from flash_attn.layers.rotary import apply_rotary_emb_func
 
 
 logger = logging.get_logger(__name__)
